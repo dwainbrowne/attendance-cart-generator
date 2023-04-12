5b8bc4184dd80fc0fc65f38141a45ecec607ba8c
Dwain Browne <dwain@snapsuite.io>
2023-04-12 05:30:36 -0400

    fix: attended property value not showing

---
+++ ConvertAdaptiveCardDataToAttendanceJson/index.js
@@ -0,0 +1,69 @@
+// Import the Luxon library for handling date and time
+const { DateTime } = require("luxon");
+
+// The main function handler for the Azure Function
+module.exports = async function (context, req) {
+    context.log('Adaptive Card Conversion HTTP trigger function processed a request.');
+
+    // Extract the input JSON from the request body
+    const inputJson = req.body;
+
+    // Check if the input JSON is provided
+    if (inputJson) {
+        // Call the formatOutputJson function to process the input JSON and create the output JSON
+        const outputJson = formatOutputJson(inputJson);
+
+        // Return the output JSON in the response
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: outputJson
+        };
+    } else {
+        // If the input JSON is not provided, return an error response
+        context.res = {
+            status: 400,
+            body: "Please pass a JSON object in the request body"
+        };
+    }
+};
+
+// Function to process the input JSON and create the output JSON structure
+function formatOutputJson(inputJson) {
+    // Initialize an empty array to store the formatted attendance data
+    const attendance = [];
+
+    // Get the current date and time using the Luxon library
+    const currentDate = DateTime.local();
+    // Initialize a counter to calculate the date for each record
+    let counter = 0;
+
+
+    // Iterate over each property (name) in the input JSON
+    for (const key in inputJson.attendance) {
+        // Check if the inputJson object has the property (key)
+        if (inputJson.attendance.hasOwnProperty(key)) {
+            // Get the value of the current property (key)
+            const attended = inputJson.attendance[key] === "true" ? true : false;
+
+            // Calculate the datetime for the current record by subtracting 'counter' days from the current date
+            const datetime = currentDate.minus({ days: counter }).toISO();
+
+            // Add the formatted attendance object to the attendance array
+            attendance.push({
+                name: key,
+                datetime: datetime,
+                attended: attended
+            });
+
+            // Increment the counter for the next record
+            counter++;
+        }
+    }
+
+    // Return the output JSON structure with the formatted attendance array
+    return {
+        id: inputJson.id,
+        meeting: inputJson.meeting,
+        attendance: attendance
+    };
+}
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # internal-node-chart
 This project is responsible for generating charts for daily and weekly attendance report in SnapSuite Teams meeting
+
+
+#Takes a json in the following format:
+```json
+{
+"id":"guid...",
+"meeting":"Developer Meeting",    
+"attendance":
+      {
+            "DwainBrowne": "true",
+            "JohnPerez": "true",
+            "LeoDiaz": "false",
+            "MarkBustria": "true",
+            "ParmSingh": "true",
+            "PavithraPackiyanathan": "true",
+            "RedmondParez": "true"
+        }
+}
+```
+
+#Returns
+```json
+{
+  "id":"guid...",
+  "meeting": "Developer Meeting",
+  "attendance": [
+    {
+      "name": "DwainBrowne",
+      "datetime": "2023-04-12T00:14:48.138-04:00"
+    },
+    {
+      "name": "JohnPerez",
+      "datetime": "2023-04-11T00:14:48.138-04:00"
+    },
+    {
+      "name": "LeoDiaz",
+      "datetime": "2023-04-10T00:14:48.138-04:00"
+    },
+    {
+      "name": "MarkBustria",
+      "datetime": "2023-04-09T00:14:48.138-04:00"
+    },
+    {
+      "name": "ParmSingh",
+      "datetime": "2023-04-08T00:14:48.138-04:00"
+    },
+    {
+      "name": "PavithraPackiyanathan",
+      "datetime": "2023-04-07T00:14:48.138-04:00"
+    },
+    {
+      "name": "RedmondParez",
+      "datetime": "2023-04-06T00:14:48.138-04:00"
+    }
+  ]
+}
+```
